f2259d863effef28132deade593a6198bb75efee
tanishqarana <tanishqa.ranaa@gmail.com>
2026-01-17 01:07:25 +0530

    Add Sepolia network config and env setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.19",
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 31337
+      chainId: 31337,
     },
     localhost: {
       url: "http://127.0.0.1:8545",
-      chainId: 31337
-    }
-  }
+      chainId: 31337,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
